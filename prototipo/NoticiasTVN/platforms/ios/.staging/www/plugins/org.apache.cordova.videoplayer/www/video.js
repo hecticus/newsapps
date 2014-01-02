a446f4b04af8ff62752b1e5d5d2e2c0826c317c8
Christian Rodriguez <christian@hecticus.com>
2014-01-02 18:40:32 -0430

    Se sube el proyecto separado de Noticias TVN con su version de android y ios

---
+++ prototipo/NoticiasTVN/platforms/ios/.staging/www/plugins/org.apache.cordova.videoplayer/www/video.js
@@ -0,0 +1,15 @@
+cordova.define("org.apache.cordova.videoplayer.VideoPlayer", function(require, exports, module) {var exec = require("cordova/exec");
+
+var VideoPlayer = function () {};
+
+/**
+ * Starts the video player intent
+ *
+ * @param url           The url to play
+ */
+VideoPlayer.prototype.play = function(url) {
+    exec(null, null, "VideoPlayer", "playVideo", [url]);
+};
+
+var videoPlayer = new VideoPlayer();
+module.exports = videoPlayer;});
