d622067be12dc9d8d7313763e799cc557235222a
Christian Rodriguez <christian@hecticus.com>
2014-01-17 17:52:21 -0430

    Arreglos al share para android de NoticiasTVN

--- NoticiasTVN/NoticiasTVN/platforms/android/.staging/www/cordova_plugins.js
+++ NoticiasTVN/NoticiasTVN/platforms/android/.staging/www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.videoplayer/www/video.js",
+        "id": "org.apache.cordova.videoplayer.VideoPlayer",
+        "clobbers": [
+            "window.videoPlayer"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.network-information/www/network.js",
         "id": "org.apache.cordova.network-information.network",
         "clobbers": [
@@ -16,27 +23,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.videoplayer/www/video.js",
-        "id": "org.apache.cordova.videoplayer.VideoPlayer",
-        "clobbers": [
-            "window.videoPlayer"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
-        "id": "org.apache.cordova.console.console",
-        "clobbers": [
-            "console"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.console/www/logger.js",
-        "id": "org.apache.cordova.console.logger",
-        "clobbers": [
-            "cordova.logger"
-        ]
-    },
-    {
         "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
         "id": "com.phonegap.plugins.PushPlugin.PushNotification",
         "clobbers": [
@@ -49,16 +35,24 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.network-information": "0.2.5",
     "org.apache.cordova.videoplayer": "1.0",
+    "org.apache.cordova.network-information": "0.2.5",
     "org.apache.cordova.console": "0.2.6",
     "com.phonegap.plugins.PushPlugin": "2.1.1",
-    "org.apache.cordova.device": "0.2.7"
+    "org.apache.cordova.device": "0.2.7",
+    "nl.x-services.plugins.socialsharing": "3.7"
 }
 // BOTTOM OF METADATA
 });
