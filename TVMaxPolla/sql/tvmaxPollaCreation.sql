b3d983018b00427c4ad92b8c7555840b5db78598
Christian Rodriguez <christian@hecticus.com>
2014-03-24 12:21:33 -0430

    TVMaxPolla: base de datos tentativa para almacenar los partidos, apuestas y resultados. Falta el enlace con los clientes sociales y con los leaderboards

---
+++ TVMaxPolla/sql/tvmaxPollaCreation.sql
@@ -0,0 +1,161 @@
+CREATE DATABASE  IF NOT EXISTS `tvmax_polla_admin` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `tvmax_polla_admin`;
+-- MySQL dump 10.13  Distrib 5.5.24, for osx10.5 (i386)
+--
+-- Host: 127.0.0.1    Database: tvmax_polla_admin
+-- ------------------------------------------------------
+-- Server version	5.1.44-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client_bet`
+--
+
+DROP TABLE IF EXISTS `client_bet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client_bet` (
+  `id_client_bet` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_client` bigint(20) NOT NULL,
+  `id_match` int(11) DEFAULT NULL,
+  `id_winner` int(11) DEFAULT NULL,
+  `score_winner` int(11) DEFAULT NULL,
+  `score_loser` varchar(45) DEFAULT NULL,
+  `id_leaderboard` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_client_bet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client_bet`
+--
+
+LOCK TABLES `client_bet` WRITE;
+/*!40000 ALTER TABLE `client_bet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_bet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `match`
+--
+
+DROP TABLE IF EXISTS `match`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `match` (
+  `id_match` int(11) NOT NULL AUTO_INCREMENT,
+  `id_team_a` int(11) NOT NULL,
+  `id_team_b` int(11) NOT NULL,
+  `id_phase` int(11) NOT NULL,
+  `date` bigint(14) DEFAULT NULL,
+  `venue` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_match`),
+  KEY `id_phase_idx` (`id_phase`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `match`
+--
+
+LOCK TABLES `match` WRITE;
+/*!40000 ALTER TABLE `match` DISABLE KEYS */;
+/*!40000 ALTER TABLE `match` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `match_results`
+--
+
+DROP TABLE IF EXISTS `match_results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `match_results` (
+  `id_match` int(11) NOT NULL,
+  `score_team_a` int(11) NOT NULL,
+  `score_team_b` int(11) NOT NULL,
+  `penalties_team_a` int(11) DEFAULT NULL,
+  `penalties_team_b` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_match`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `match_results`
+--
+
+LOCK TABLES `match_results` WRITE;
+/*!40000 ALTER TABLE `match_results` DISABLE KEYS */;
+/*!40000 ALTER TABLE `match_results` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phase`
+--
+
+DROP TABLE IF EXISTS `phase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phase` (
+  `id_phase` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `date_start` bigint(14) DEFAULT NULL,
+  `date_end` bigint(14) DEFAULT NULL,
+  PRIMARY KEY (`id_phase`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phase`
+--
+
+LOCK TABLES `phase` WRITE;
+/*!40000 ALTER TABLE `phase` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `id_team` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `shortname` varchar(45) DEFAULT NULL,
+  `flag_file` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_team`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team`
+--
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-24 12:17:24
