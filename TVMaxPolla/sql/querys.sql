f2c3b8e1d6d9977c16cb9625eedede3a9c9271fa
ali.buenano <alidanielbs@gmail.com>
2014-03-26 15:22:20 -0500

    Se actualiza el dump con todo los juegos

--- TVMaxPolla/sql/querys.sql
+++ TVMaxPolla/sql/querys.sql
@@ -7,8 +7,8 @@ WHERE G.name LIKE '%g%';
 SELECT M.id_match, G.name as _group, TA.name as home, Tb.name as visit, P.name, V.name, DATE_FORMAT(M.date,'%Y-%m-%d %h:%i:%s') as date
 FROM tvmax_polla_admin.match AS M
 INNER JOIN tvmax_polla_admin.group AS G ON M.id_group = G.id_group
-INNER JOIN tvmax_polla_admin.team AS TA ON TA.id_team = M.id_team_a
-INNER JOIN tvmax_polla_admin.team AS TB ON TB.id_team = M.id_team_b
+LEFT JOIN tvmax_polla_admin.team AS TA ON TA.id_team = M.id_team_a
+LEFT JOIN tvmax_polla_admin.team AS TB ON TB.id_team = M.id_team_b
 INNER JOIN tvmax_polla_admin.phase AS P ON P.id_phase = M.id_phase
 INNER JOIN tvmax_polla_admin.venue AS V ON V.id_venue = M.id_venue
-WHERE G.name LIKE '%h%';
+WHERE G.name LIKE '%a%';
