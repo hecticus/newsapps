74d30f12108b88f065e9123948831adaafa869e7
ali.buenano <alidanielbs@gmail.com>
2014-04-09 15:37:03 -0500

    Se actualiza neuvos elementos propuestos hoy

--- NoticiasTVN/NoticiasTVN/platforms/android/assets/www/js/index_tvn.js
+++ NoticiasTVN/NoticiasTVN/platforms/android/assets/www/js/index_tvn.js
@@ -799,16 +799,21 @@ function initBasicApp(){
 
 								$.li='<li data-view="trending" >';
 								
-									$.li+='<div style="position:relative; display:inline-block; width:'+(((viewport.width*20)/100))+'px; height:auto; min-height:70px; max-height:70px; float:left; background-color: #034985; color:#ffffff; font-style:italic; vertical-align:bottom; box-sizing:border-box;">';
-									$.li+='<div style="position:absolute; top:35%; text-align:center;">';									
-									$.li+='<span style="font-size:1.0em;">Tendencias</span> <span style="font-size:1.2em; font-weight:bold;">DE HOY</span>';
-									$.li+='</div>';
+									$.li+='<div style="position:relative; display:inline-block; width:'+(((viewport.width*30)/100))+'px; height:auto; min-height:70px; max-height:70px; float:left; background-color: #034985; color:#ffffff; font-style:italic; vertical-align:bottom; box-sizing:border-box;">';
+									
+									$.li+='<div style="position: absolute; top: 50%; left: 50%; height: 30%; width:100%; margin: -20% 0 0 -30%;">';
+										$.li+='<span style="font-size:1.0em;">Tendencias</span> <br />';
+										$.li+='<span style="font-size:1.4em; font-weight:bold;">DE HOY</span>';
 									$.li+='</div>';
 									
-									$.li+='<div style="width:'+((viewport.width*80)/100)+'px; height:auto; float:left;">';
+
+									
+									$.li+='</div>';
+									
+									$.li+='<div style="width:'+((viewport.width*70)/100)+'px; height:auto; float:left;">';
 
 									arrTrendingTopics.forEach(function(trending,i) {
-										$.li+='<div style="width:'+(((viewport.width*40)/100))+'px; height:auto; min-height:35px; max-height:35px; float:left; background-color:#f9f9f9;  border-left:1px  solid #ffffff;  border-bottom:1px  solid #ffffff; vertical-align:top; padding:2px; box-sizing:border-box;">';
+										$.li+='<div style="width:'+(((viewport.width*35)/100))+'px; height:auto; min-height:35px; max-height:35px; float:left; background-color:#f9f9f9;  border-left:1px  solid #ffffff;  border-bottom:1px  solid #ffffff; vertical-align:top; padding:2px; box-sizing:border-box;">';
 										$.li+='<p class="trending" data-content="trending" data-id="'+trending.categoria+'" style="color:#ffffff; text-align: left; font-size:1.2em; font-weight:bold; color:#999999; display:inline; ">#'+trending.titulo+'</p>';										
 										$.li+='</div>';
 									});
@@ -816,8 +821,11 @@ function initBasicApp(){
 									$.li+='</div>';
 									
 								$.li+='</li>';
-										
-
+								
+								$.li+='<li data-view="banner" >';
+									$.li+='<img src="img/banner/large.jpg" style="width:100%; height:auto; " />';						
+								$.li+='</li>';
+											
 																								
 								$($.category +'-news1').append($.li);
 								
