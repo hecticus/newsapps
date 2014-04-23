92ef7ce1e055b2d5f5b572bdb695c2bc8bc40877
ali.buenano <alidanielbs@gmail.com>
2014-04-23 09:04:45 -0500

    Maquetacion de Casa de Baterias

--- CasaBaterias/CasaBaterias/platforms/android/assets/www/js/index.js
+++ CasaBaterias/CasaBaterias/platforms/android/assets/www/js/index.js
@@ -16,6 +16,29 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+
+var map; 
+var myLatlng; 
+function initialize() {
+
+	var latitud = jQuery('#maps-sucursal option:selected').data('latitud');
+	var longitud = jQuery('#maps-sucursal option:selected').data('longitud');
+	
+    myLatlng = new google.maps.LatLng(latitud, longitud); 
+    var mapOptions = { 
+        zoom: 16, 
+        center: myLatlng 
+    }; 
+    map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions); 
+} 
+var marker = new google.maps.Marker({ 
+    position: myLatlng, 
+    map: map, 
+    title: 'Mi punto en el mapa' 
+});
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,17 +56,124 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    	
+		document.addEventListener('backbutton', function checkConnection() {
+			
+    		$(function() { 			  
+ 			  	$('#content').attr('class','page transition right');
+ 			  	myScrollMenu.scrollTo(0,0,0);
+			});
+			
+    	}, false);
+    	
+    	
+    	google.maps.event.addDomListener(document.getElementById('maps-sucursal'), 'change', initialize); 
+    	document.addEventListener('touchmove', function (e) {e.preventDefault();}, false);    	
+    	document.body.addEventListener('touchmove', function(event) {event.preventDefault();}, false);    	 
         app.receivedEvent('deviceready');
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    	
+    	
+    	
+    	
+    	
+    	
+    	var _hoverTimeout = null;
+       	
+       	var myScrollDelivery, myScrollMenu, myScrollMaps;
+       	
+       	myScrollDelivery = new IScroll('#delivery'); 
+       	
+       	      
+		myScrollMaps = new IScroll('#maps'); 
+     
+     
+     
+       	
+       	myScrollMenu = new IScroll('#index',{tap:true});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		myScrollMenu.on('scrollStart', function () {
+			clearTimeout(_hoverTimeout);
+			$('.option').removeClass('hover');
+		});
 
-        console.log('Received Event: ' + id);
+
+		myScrollMenu.on('scrollCancel', function () {
+			clearTimeout(_hoverTimeout);	
+			$('.option').removeClass('hover');
+		});
+
+	
+		$(function() {
+	       		
+	       		
+	       		
+       		$('input, textarea','select').on('focus', function (e) {
+			    $('header, footer').css('position', 'absolute');
+			}).on('blur', function (e) {
+			    $('header, footer').css('position', 'fixed');
+			
+			});
+	       		
+	       	$.getJSON('json/maps.json', function(maps){
+    			$.each(maps.pais, function(i,pais){    			
+    				$('#maps-country').append('<option value="'+pais.nombre+'">&nbsp;'+pais.nombre+'</option>');
+    			});    			
+    		});
+	       	
+	       	$('#maps-country').on('change', function (e){
+	       		
+	       		var optionSelected = $("option:selected", this);
+    			var valueSelected = this.value;
+	       		$('#maps-sucursal').empty();
+	       		$('#map-canvas').empty();
+	       		
+	       		
+			  	$.getJSON('json/maps.json', function(maps){
+	    			$.each(maps.pais, function(i,pais){	    				
+	    				if (valueSelected == pais.nombre) {
+	    					$('#maps-sucursal').append('<option value="">&nbsp;Seleccione</option>');
+	    					$.each(pais.sucursal, function(i,sucursal){
+	    						$('#maps-sucursal').append('<option data-latitud="'+sucursal.latitud+'" data-longitud="'+sucursal.longitud+'"  value="'+sucursal.nombre+'">&nbsp;'+sucursal.nombre+'</option>');
+	    					});
+	    				}
+	    			});    			
+    			});
+    			
+			});
+	       	
+
+			$(document).on('touchend','.btn-send', function() {				
+				$("form").find(':input:not([type="button"])').each(function() {
+        			
+        		});
+			}); 
+	
+       		$(document).on('touchend','header', function() {      			
+       			$('#delivery,#maps').attr('class','page transition right');
+       			myScrollMenu.scrollTo(0,0,0);		
+			});
+			
+			
+			$(document).on('tap','.option', function() {
+				var option = $(this).data('option');				
+				$(option).attr('class','page transition left');
+			});
+
+	       	$(document).on('touchstart','.option', function() {
+				var _this = this;
+	       		clearTimeout(_hoverTimeout);
+				_hoverTimeout = setTimeout(function () {
+					$(_this).addClass('hover');													
+				}, 10);
+			});
+	       		
+
+	       		
+		});
+       
     }
 };
