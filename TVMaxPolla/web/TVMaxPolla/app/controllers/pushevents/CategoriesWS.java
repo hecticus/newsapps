338463947ab2096327d399d2567d1f70c3a8a874
Inaki Plessmann <inaki@hecticus.com>
2014-06-04 09:55:22 -0430

    WS para insertar y eliminar acciones y categorías

--- TVMaxPolla/web/TVMaxPolla/app/controllers/pushevents/CategoriesWS.java
+++ TVMaxPolla/web/TVMaxPolla/app/controllers/pushevents/CategoriesWS.java
@@ -3,13 +3,16 @@ package controllers.pushevents;
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.EbeanServer;
 import controllers.HecticusController;
+import models.pushevents.Action;
 import models.pushevents.Category;
 import models.pushevents.CategoryClient;
+import models.pushevents.ClientAction;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.node.ObjectNode;
 import play.mvc.Result;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by plesse on 5/27/14.
@@ -21,17 +24,76 @@ public class CategoriesWS extends HecticusController {
             ObjectNode jsonInfo = getJson();
             Long idClient  = jsonInfo.get("idClient").asLong();
 
-            Iterator<JsonNode> cats = jsonInfo.get("categories").getElements();
-            long creationTime = System.currentTimeMillis();
-            while(cats.hasNext()){
-                JsonNode actual = cats.next();
-                Long idCategory = actual.get("category").asLong();
-                Category category = Category.finder.byId(idCategory);
-                if(category != null){
-                    CategoryClient act = CategoryClient.finder.where().eq("idClient", idClient).eq("id_category", idCategory).findUnique();
-                    if(act == null){
-                        act = new CategoryClient(idClient, category, creationTime);
-                        act.save();
+            if(jsonInfo.has("categories")){
+                Iterator<JsonNode> cats = jsonInfo.get("categories").getElements();
+                long creationTime = System.currentTimeMillis();
+                while(cats.hasNext()){
+                    JsonNode actual = cats.next();
+                    Long idCategory = actual.get("category").asLong();
+                    Category category = Category.finder.byId(idCategory);
+                    if(category != null){
+                        CategoryClient act = CategoryClient.finder.where().eq("idClient", idClient).eq("id_category", idCategory).findUnique();
+                        if(act == null){
+                            act = new CategoryClient(idClient, category, creationTime);
+                            act.save();
+                        }
+                    }
+                }
+                if(!jsonInfo.has("insertActions")){
+                    List<Action> action = Action.finder.where().eq("pushable", 1).findList();
+                    for(Action a : action){
+                        ClientAction act = ClientAction.finder.where().eq("idClient", idClient).eq("id_action", a.getIdAction()).findUnique();
+                        if(act == null){
+                            act = new ClientAction(idClient, a);
+                            act.save();
+                        }
+                    }
+                }
+            }
+
+            if(jsonInfo.has("deleteCategories")){
+                Iterator<JsonNode> cats = jsonInfo.get("deleteCategories").getElements();
+                while(cats.hasNext()){
+                    JsonNode actual = cats.next();
+                    Long idCategory = actual.get("category").asLong();
+                    Category category = Category.finder.byId(idCategory);
+                    if(category != null){
+                        CategoryClient act = CategoryClient.finder.where().eq("idClient", idClient).eq("id_category", idCategory).findUnique();
+                        if(act != null){
+                            act.delete();
+                        }
+                    }
+                }
+            }
+
+
+            if(jsonInfo.has("insertActions")){
+                Iterator<JsonNode> acts = jsonInfo.get("insertActions").getElements();
+                while(acts.hasNext()){
+                    JsonNode actual = acts.next();
+                    Long idAction = actual.get("action").asLong();
+                    Action action = Action.finder.byId(idAction);
+                    if(action != null){
+                        ClientAction act = ClientAction.finder.where().eq("idClient", idClient).eq("id_action", idAction).findUnique();
+                        if(act == null){
+                            act = new ClientAction(idClient, action);
+                            act.save();
+                        }
+                    }
+                }
+            }
+
+            if(jsonInfo.has("deleteActions")){
+                Iterator<JsonNode> acts = jsonInfo.get("deleteActions").getElements();
+                while(acts.hasNext()){
+                    JsonNode actual = acts.next();
+                    Long idAction = actual.get("action").asLong();
+                    Action action = Action.finder.byId(idAction);
+                    if(action != null){
+                        ClientAction act = ClientAction.finder.where().eq("idClient", idClient).eq("id_action", idAction).findUnique();
+                        if(act != null){
+                            act.delete();
+                        }
                     }
                 }
             }
@@ -54,6 +116,55 @@ public class CategoriesWS extends HecticusController {
                     CategoryClient act = CategoryClient.finder.where().eq("idClient", idClient).eq("id_category", idCategory).findUnique();
                     if(act != null){
                         act.delete();
+                    }
+                }
+            }
+            return ok(hecticusResponseSimple(0,"ok", null,null));
+        } catch (Exception e){
+            return badRequest("Ocurrio un error insertando el cliente " + e.getMessage());
+        }
+    }
+
+    public static Result insertActions(){
+        try{
+            ObjectNode jsonInfo = getJson();
+            Long idClient  = jsonInfo.get("idClient").asLong();
+            if(jsonInfo.has("actions")){
+                Iterator<JsonNode> acts = jsonInfo.get("actions").getElements();
+                while(acts.hasNext()){
+                    JsonNode actual = acts.next();
+                    Long idAction = actual.get("action").asLong();
+                    Action action = Action.finder.byId(idAction);
+                    if(action != null){
+                        ClientAction act = ClientAction.finder.where().eq("idClient", idClient).eq("id_action", idAction).findUnique();
+                        if(act == null){
+                            act = new ClientAction(idClient, action);
+                            act.save();
+                        }
+                    }
+                }
+            }
+            return ok(hecticusResponseSimple(0,"ok", null,null));
+        } catch (Exception e){
+            return badRequest("Ocurrio un error insertando el cliente " + e.getMessage());
+        }
+    }
+
+    public static Result deleteActions(){
+        try{
+            ObjectNode jsonInfo = getJson();
+            Long idClient  = jsonInfo.get("idClient").asLong();
+            if(jsonInfo.has("actions")){
+                Iterator<JsonNode> acts = jsonInfo.get("actions").getElements();
+                while(acts.hasNext()){
+                    JsonNode actual = acts.next();
+                    Long idAction = actual.get("action").asLong();
+                    Action action = Action.finder.byId(idAction);
+                    if(action != null){
+                        ClientAction act = ClientAction.finder.where().eq("idClient", idClient).eq("id_action", idAction).findUnique();
+                        if(act != null){
+                            act.delete();
+                        }
                     }
                 }
             }
---
+++ TVMaxPolla/web/TVMaxPolla/app/models/pushevents/Action.java
@@ -0,0 +1,68 @@
+package models.pushevents;
+
+import models.HecticusModel;
+import org.codehaus.jackson.node.ObjectNode;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+import play.libs.Json;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by plesse on 6/3/14.
+ */
+@Entity
+@Table(name="actions")
+public class Action extends HecticusModel {
+    @Override
+    public ObjectNode toJson() {
+        ObjectNode responseNode = Json.newObject();
+        responseNode.put("idAction", idAction);
+        responseNode.put("name", name);
+        responseNode.put("template", template);
+        responseNode.put("pushable", pushable);
+        List<ObjectNode> clients = new ArrayList<ObjectNode>(this.clients.size());
+        for(int i = 0; i < clients.size(); i++){
+            clients.add(this.clients.get(i).toJson());
+        }
+        responseNode.put("clients", Json.toJson(clients));
+        return responseNode;
+    }
+
+    @Id
+    private Long idAction;
+    @Constraints.Required
+    private String name;
+    @Constraints.Required
+    private String template;
+    @Constraints.Required
+    private Integer pushable;
+
+    @OneToMany(mappedBy = "action")
+    private List<ClientAction> clients;
+
+    public static Model.Finder<Long, Action> finder = new
+            Model.Finder<Long, Action>(Long.class, Action.class);
+
+
+    public Long getIdAction() {
+        return idAction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public Integer getPushable() {
+        return pushable;
+    }
+}
