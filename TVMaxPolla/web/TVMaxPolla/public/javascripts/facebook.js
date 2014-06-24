1b3e2383e9cf06acd63aba803c9204cfb7e27d4a
ali.buenano <alidanielbs@gmail.com>
2014-06-24 13:53:26 -0500

    Control del error -FB.getLoginStatus() called before calling FB.init()-

--- TVMaxPolla/web/TVMaxPolla/public/javascripts/facebook.js
+++ TVMaxPolla/web/TVMaxPolla/public/javascripts/facebook.js
@@ -13,9 +13,9 @@
 	  var js, fjs = d.getElementsByTagName(s)[0];
 	  if (d.getElementById(id)) return;
 	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=647787605309095";
-	  fjs.parentNode.insertBefore(js, fjs);	 	  
-		   
+	  //js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&appId=647787605309095";
+	  js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1&status=0";	  
+	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
 	var login_event = function(response) {			
