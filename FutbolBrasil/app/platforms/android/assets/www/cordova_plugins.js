0886c129b9f6070e6de93917c141c42e75ef2160
ali.buenano <alidanielbs@gmail.com>
2014-09-29 17:53:11 -0500

    Actualizaciones y test de api de facebook

--- FutbolBrasil/app/platforms/android/assets/www/cordova_plugins.js
+++ FutbolBrasil/app/platforms/android/assets/www/cordova_plugins.js
@@ -216,6 +216,13 @@ module.exports = [
         "clobbers": [
             "window.plugins.socialsharing"
         ]
+    },
+    {    			 
+        "file": "plugins/com.phonegap.plugins.facebookconnect.FacebookConnectPlugin/www/phonegap/plugin/facebookConnectPlugin/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "window.facebookConnectPlugin"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -230,7 +237,8 @@ module.exports.metadata =
     "org.apache.cordova.file-transfer": "0.4.6",
     "com.jsmobile.plugins.sms": "0.0.1",
     "org.apache.cordova.videoplayer": "1.0",
-    "nl.x-services.plugins.socialsharing": "4.3.6"
+    "nl.x-services.plugins.socialsharing": "4.3.6",
+    "com.phonegap.plugins.facebookconnect": "0.5.1"
 }
 // BOTTOM OF METADATA
 });
