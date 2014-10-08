8c3f4585d4865ecc6ba9ae3a72ab7d173efdd011
ali.buenano <alidanielbs@gmail.com>
2014-10-08 17:10:27 -0500

    Asocion de imagenes a una noticia

--- FutbolBrasil/web/app/controllers/Application.java
+++ FutbolBrasil/web/app/controllers/Application.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import models.*;
 
@@ -14,6 +17,25 @@ import views.html.*;
 
 public class Application extends Controller {
 
+	public static ObjectNode getJson(){
+        ObjectNode jsonInfo = (ObjectNode) request().body().asJson();
+        if(jsonInfo == null){
+            Map<String,String[]> b = request().body().asFormUrlEncoded();
+            if(b == null){
+                return null;
+            }
+            jsonInfo = Json.newObject();
+            Set<String> keys = b.keySet();
+            Iterator<String> it = keys.iterator();
+            while(it.hasNext()){
+                String key = (String)it.next();
+                jsonInfo.put(key, Json.toJson(b.get(key)[0]));
+            }
+        }
+        return jsonInfo;
+    }
+	
+	
     public static Result index(int page, String sortBy, String order, String filter) {
     	return ok(
     		index.render(News.page(page, 6, sortBy, order, filter),
@@ -44,6 +66,7 @@ public class Application extends Controller {
     public static Result getNewsById(Long id) {    	
     	News objNews = News.getNewsById(id);    	
     	List<Resource> lstResource = Resource.getAllResourcesAvailable();        
+    	//List<Resource> lstResource = Resource.getAllResource();
         return ok(summary.render(objNews,lstResource));        
     }
     
@@ -68,6 +91,28 @@ public class Application extends Controller {
         
     }
     
+    public static Result upResource() {
+    	
+    	ObjectNode jData = getJson();
+    	News objNews = new News();
+    	Resource objResource = new Resource();
+    	List<Resource> lstResource = new ArrayList<Resource>();
+
+    	 for (int i = 0; i < jData.get("resources").size(); i++){
+    		 objResource = Resource.getResource(jData.get("resources").get(i).asLong());
+    		 lstResource.add(objResource);    	                          
+         }
+
+    	objNews = News.getNewsById(jData.get("news").asLong());
+    	objNews.setResources(lstResource);
+    	objNews.update();
+     
+	 	ObjectNode jResponse = Json.newObject();
+	    jResponse.put("response",Json.toJson((objNews)));
+	    
+	    return ok(jResponse);
+   	 
+   }
     
 
 }
