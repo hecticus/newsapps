bda2510f3622a4f3e923eb31d72b762fe289c256
ali.buenano <alidanielbs@gmail.com>
2014-11-14 15:01:56 -0500

    AGATADODIA: se implementa l&f de inicio

--- AGataDoDia/AGataDoDia/AGataDoDia/platforms/android/assets/www/js/signin.js
+++ AGataDoDia/AGataDoDia/AGataDoDia/platforms/android/assets/www/js/signin.js
@@ -3,8 +3,7 @@
 	var _oAjax = _fGetAjaxJson(_url + '/garotas/loading/'+$(window).width()+'/'+$(window).height());	
 	if (_oAjax) {		
 		_oAjax.done(function(_json) {
-			var image = _json.response.feature_image.link;
-			console.log("image:"+image);
+			var image = _json.response.feature_image.link;	
 			if(image != null){
 				document.body.style.backgroundImage="url('"+image+"')";
 			}
