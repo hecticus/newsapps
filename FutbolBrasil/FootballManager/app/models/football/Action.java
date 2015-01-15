9a8909ce82c4df895e2bc65931243c7ff9628585
Inaki Plessmann <inaki@hecticus.com>
2015-01-15 10:47:47 -0430

    FOOTBALL_MANAGER: WS y parser de min a min

--- FutbolBrasil/FootballManager/app/models/football/Action.java
+++ FutbolBrasil/FootballManager/app/models/football/Action.java
@@ -12,14 +12,14 @@ import java.util.List;
  * Created by karina on 5/20/14.
  */
 @Entity
-@Table(name="actions",
-       uniqueConstraints = @UniqueConstraint(columnNames = {"mnemonic"}))
+@Table(name="actions", uniqueConstraints = @UniqueConstraint(columnNames = {"extId"}))
 public class Action extends HecticusModel {
 
     @Id
     private Integer idActions;
     private String mnemonic;
     private String description;
+    private Integer extId;
     //private Integer ext_id;
     @OneToMany(mappedBy = "action")
     private List<GameMatchEvent> events;
@@ -28,9 +28,10 @@ public class Action extends HecticusModel {
 
     public Action(){}
 
-    public Action(String mnemonic, String description) {
+    public Action(String mnemonic, String description, Integer extId) {
         this.mnemonic = mnemonic;
         this.description = description;
+        this.extId = extId;
     }
 
     public Integer getIdActions() {
@@ -61,8 +62,28 @@ public class Action extends HecticusModel {
         return finder.byId(id);
     }
 
+    public Integer getExtId() {
+        return extId;
+    }
+
+    public void setExtId(Integer extId) {
+        this.extId = extId;
+    }
+
+    public List<GameMatchEvent> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<GameMatchEvent> events) {
+        this.events = events;
+    }
+
     public static Action findByMnemonic(String mnemonic){
         return  finder.where().eq("mnemonic",mnemonic).findUnique();
+    }
+
+    public static Action findByExtId(int extId){
+        return  finder.where().eq("extId",extId).findUnique();
     }
 
     @Override
@@ -75,12 +96,13 @@ public class Action extends HecticusModel {
     }
 
     public void validate() {
-        Action tr = findByMnemonic(this.mnemonic);
+        Action tr = findByExtId(this.extId);
         if (tr != null) {
             //existe
             this.idActions = tr.idActions;
             this.description = tr.description;
             this.mnemonic = tr.mnemonic;
+            this.extId = tr.extId;
         } else {
             this.save();
         }
