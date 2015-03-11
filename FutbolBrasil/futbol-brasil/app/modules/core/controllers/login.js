b02991c075a8ab328c08f72a5b4457be29898804
Fernando De Freitas <fernando.defreitas@hecticus.com>
2015-03-11 11:46:36 -0430

    FUTBOL BRASIL:  - Avances en rediseño de vista de Login - Avances en Guest Mode

--- FutbolBrasil/futbol-brasil/app/modules/core/controllers/login.js
+++ FutbolBrasil/futbol-brasil/app/modules/core/controllers/login.js
@@ -13,15 +13,21 @@ angular
 
             //TODO i18n-alizar
             $scope.strings = {
-                PASSWORD_HOLDER: 'Senha',
-                PASSWORD_LABEL: 'Digite a senha recebida por SMS.',
+                PASSWORD_LABEL: 'Senha',
+                PASSWORD_HELPER: 'Digite a senha recebida por SMS.',
                 LOADING_MESSAGE: 'Carregando...',
                 START_TRIAL_MESSAGE: 'Experimente 7 dias grátis',
                 SEND_MESSAGE: 'Enviar',
                 RESEND_MESSAGE: 'Enviar novamente a senha',
                 MSISDN_HELPER: 'Digite seu numero de celular.',
                 MSISDN_HOLDER: '# Numero',
-                LOGIN_WELCOME_MESSAGE: 'Registre-se para acessar as notícias de futebol do dia, todos os dias.'
+                MSISDN_LABEL: 'Username',
+                LOGIN_LABEL: 'Login',
+                LOGIN_WELCOME_MESSAGE: 'Registre-se para acessar as notícias de futebol do dia, todos os dias.',
+                REMIND_LABEL : 'Remind / Get Credentials',
+                CHANGE_LANGUAGE_LABEL : 'Change Language',
+                TUTORIAL_LABEL : 'How Does It Work?',
+                ENTER_AS_GUEST_LABEL: 'Enter as Guest'
             };
             $scope.msisdn = '';
             $scope.password = '';
