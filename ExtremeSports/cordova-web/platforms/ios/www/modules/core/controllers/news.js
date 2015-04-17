4d2e3c0db10eda08904dbf768c069c0b41a41418
Jose Ortegano <jose.ortegano@hecticus.com>
2015-04-17 16:53:54 -0430

    EXTREME_SPORTS: Se agrega "platform" ios

---
+++ ExtremeSports/cordova-web/platforms/ios/www/modules/core/controllers/news.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * @ngdoc object
+ * @name core.Controllers.NewsController
+ * @description NewsController
+*/
+angular.module('core').controller('NewsController', NewsController);
+
+NewsController.$inject = ['$scope', '$state', 'News', 'iScroll'];
+
+function NewsController($scope, $state, News, iScroll) {
+
+    var vm = this;
+    var scroll = null;
+
+    vm.posts = [];
+    vm.openDetail = openDetail;
+
+    init();
+
+    /*//////// Function Implementations  ////////*/
+
+    function openDetail(post){
+        $state.go('news-detail', {'newsId' : post.id_post});
+    }
+
+    function getPostsBefore(post){
+
+    }
+
+    function getPostsAfter(post){
+
+    }
+
+    function getPosts(){
+        News.get().then(success, error);
+
+        function success(data){
+            vm.posts = data;
+            console.log('getPosts. success.');
+            console.log(vm.posts);
+        }
+
+        function error(data){
+            vm.posts = [];
+            console.log('getPosts. error.');
+        }
+    }
+
+    function setUpIScroll(){
+        scroll = iScroll.vertical('news-wrapper');
+        $scope.$on('$destroy', function() {
+            scroll.destroy();
+            scroll = null;
+        });
+    }
+
+    function init(){
+        setUpIScroll();
+        getPosts()
+    }
+}
