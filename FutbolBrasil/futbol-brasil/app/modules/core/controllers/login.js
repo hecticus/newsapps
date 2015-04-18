f8e3e08a4758bc4cc483fa8095ea408e835361f8
ali.buenano <alidanielbs@gmail.com>
2015-04-18 10:56:34 -0500

    FUTBOL BRASIL: Mejoras l&f MTM y Loading del login & Remind

--- FutbolBrasil/futbol-brasil/app/modules/core/controllers/login.js
+++ FutbolBrasil/futbol-brasil/app/modules/core/controllers/login.js
@@ -137,7 +137,7 @@ angular
                                 , true, remindSuccess, remindError);
                         },
                         function(){
-                            $scope.$emit('unload');
+                            //$scope.$emit('unload');
                             console.log('Error saving MSISDN');
                         }
                     );
@@ -149,7 +149,7 @@ angular
                         type: 'warning'
                     });
                 }
-                $scope.$emit('unload');
+                //$scope.$emit('unload');
             };
 
             $scope.doMsisdnLogin = function(){
@@ -170,7 +170,7 @@ angular
                         type: 'warning'
                     });
                 }
-                $scope.$emit('unload');
+               // $scope.$emit('unload');
             };
 
             $scope.enterAsGuest = function(){
