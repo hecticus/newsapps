864800b788b5ca17d505e2b575cf6d468a437b3d
Iñaki Plessmann <inaki@hecticus.com>
2015-06-05 09:22:09 -0430

    FOOTBALL_(BRAZIL|MANAGER): limpieza de dependencias

--- FutbolBrasil/web/app/controllers/FootballController.java
+++ FutbolBrasil/web/app/controllers/FootballController.java
@@ -22,48 +22,5 @@ public class FootballController extends HecticusController {
         return responseNode;
     }
 
-    public static play.libs.Jsonp buildBasicJSONPResponse(int code, String responseMsg, ObjectNode obj) {
-        String callback = getJSONPCallback();
-        ObjectNode responseNode = buildBasicResponse(code, responseMsg, obj);
-        return jsonp(callback, responseNode);
-    }
 
-    public static ObjectNode buildBasicResponse(int code, String responseMsg, ObjectNode obj) {
-        ObjectNode responseNode = Json.newObject();
-        responseNode.put(Config.ERROR_KEY, code);
-        responseNode.put(Config.DESCRIPTION_KEY, responseMsg);
-        responseNode.put(Config.RESPONSE_KEY,obj);
-        return responseNode;
-    }
-
-    public static play.libs.Jsonp buildBasicJSONPResponse(int code, String responseMsg, JsonNode obj) {
-        String callback = getJSONPCallback();
-        ObjectNode responseNode = buildBasicResponse(code, responseMsg, obj);
-        return jsonp(callback, responseNode);
-    }
-
-    public static ObjectNode buildBasicResponse(int code, String responseMsg, JsonNode obj) {
-        ObjectNode responseNode = Json.newObject();
-        responseNode.put(Config.ERROR_KEY, code);
-        responseNode.put(Config.DESCRIPTION_KEY, responseMsg);
-        responseNode.put(Config.RESPONSE_KEY,obj);
-        return responseNode;
-    }
-
-    public static String getJSONPCallback(){
-        String callback = request().queryString().get("callback")[0];
-        return callback;
-    }
-
-    public static play.libs.Jsonp buildBasicJSONPResponse(int code, String responseMsg) {
-        String callback = getJSONPCallback();
-        ObjectNode responseNode = buildBasicResponse(code, responseMsg);
-        return jsonp(callback, responseNode);
-    }
-
-    public static play.libs.Jsonp buildBasicJSONPResponse(int code, String responseMsg, Exception e) {
-        String callback = getJSONPCallback();
-        ObjectNode responseNode = buildBasicResponse(code, responseMsg, e);
-        return jsonp(callback, responseNode);
-    }
 }
--- FutbolBrasil/web/build.sbt
+++ FutbolBrasil/web/build.sbt
@@ -18,10 +18,7 @@ libraryDependencies ++= Seq(
   javaWs,
   "be.objectify"  %% "deadbolt-java"     % "2.3.0-RC1",
   "com.feth"      %% "play-authenticate" % "0.6.5-SNAPSHOT",
-  "mysql" % "mysql-connector-java" % "5.1.26",
-  "com.google.guava" % "guava" % "15.0",
-	"net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.1.0",
-	"org.apache.commons" % "commons-io" % "1.3.2",
+  "org.apache.commons" % "commons-io" % "1.3.2",
 	"org.apache.jclouds.driver" % "jclouds-slf4j" % "1.8.0",
 	"org.apache.jclouds.driver" % "jclouds-sshj" % "1.8.0",
 	"org.apache.jclouds.provider" % "rackspace-cloudservers-us" % "1.8.0",
@@ -34,14 +31,7 @@ libraryDependencies ++= Seq(
 	"org.apache.jclouds.provider" % "rackspace-cloudblockstorage-uk" % "1.8.0",
 	"org.apache.jclouds.provider" % "rackspace-clouddns-uk" % "1.8.0",
 	"org.apache.jclouds.provider" % "rackspace-cloudloadbalancers-uk" % "1.8.0",
-	"org.apache.jclouds" % "jclouds-compute" % "1.8.0",
-	"org.reflections" % "reflections" % "0.9.7.RC1",
-	"javax.activation" % "activation" % "1.1",
-	"javax.mail" % "mail" % "1.4.7",
-	"com.sun.xml.messaging.saaj" % "saaj-impl" % "1.3",
-	"com.typesafe.akka" %% "akka-actor" % "2.3.3",
-	"com.typesafe.akka" %% "akka-contrib" % "2.3.3",
-	"com.typesafe.akka" %% "akka-remote" % "2.3.4"
+	"org.apache.jclouds" % "jclouds-compute" % "1.8.0"
 )
 
 resolvers ++= Seq(
